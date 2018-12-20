09a39a166ea3496e6becd9fea6d3d53e07c1b36b
Changyu007 <42179470+Changyu007@users.noreply.github.com>
2018-12-20 03:40:49 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>研究生课程推荐表</title>
+    <script type="text/javascript" src="js/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="js/courses.js"></script>
+</head>
+
+<body>
+    <?php
+        echo "<script>document.write("abc");</script>";
+    ?>
+</body>
+
+</html>
